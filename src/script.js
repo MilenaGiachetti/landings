a582bb84d5c6c778f31ed4e7e2c6c9d7205336f4
Milena Giachetti <giachettimilena@gmail.com>
2021-04-13 20:01:13 -0300

    Add card masonry base

--- src/script.js
+++ src/script.js
@@ -1,2 +1,14 @@
 import "./style.scss";
-console.log('hi');
+
+let cards = document.getElementsByClassName("card");
+function resizeGridItems() {
+    console.log("here2");
+    for (let i = 0; i < cards.length; i ++){
+        let height = cards[i].querySelector(".card-body").getBoundingClientRect().height;
+        console.log(height);
+        let rows = Math.ceil(height / 20);
+        cards[i].style.gridRowEnd = 'span ' + rows;
+    }
+}
+resizeGridItems();
+window.addEventListener("resize",  () => resizeGridItems());
