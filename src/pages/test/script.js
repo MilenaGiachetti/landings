e873e292cd1e82936f29a2cecc32af0a67f3fd12
Milena Giachetti <giachettimilena@gmail.com>
2021-04-13 19:55:27 -0300

    Add room homepage layout base with grid

--- src/pages/test/script.js
+++ src/pages/test/script.js
@@ -2,19 +2,23 @@ import "./style.scss";
 
 const next_slide = document.getElementById("next_slide");
 const previous_slide = document.getElementById("previous_slide");
-const slides = document.getElementsByClassName("slide");
+const slides_image = document.getElementsByClassName("img_ctn");
+const slides_text = document.getElementsByClassName("txt_ctn");
 let currentSlide = 0;
+const total_slides = slides_image.length;
 
 function change_slide (change) {
-    slides[currentSlide].classList.add("hidden");
-    if ((currentSlide + change) === (slides.length)) {
+    slides_image[currentSlide].classList.add("hidden");
+    slides_text[currentSlide].classList.add("hidden");
+    if ((currentSlide + change) === (total_slides)) {
         currentSlide = 0;
     } else if ((currentSlide + change) < 0) {
-        currentSlide = slides.length - 1;
+        currentSlide = total_slides - 1;
     } else {
         currentSlide += change;
     }
-    slides[currentSlide].classList.remove("hidden");
+    slides_image[currentSlide].classList.remove("hidden");
+    slides_text[currentSlide].classList.remove("hidden");
 }
 
 next_slide.addEventListener('click', ()=>change_slide(1));
