60b6779ae5750efbc1fb9e4fb209d06c167b83ee
Milena Giachetti <giachettimilena@gmail.com>
2021-04-13 09:50:55 -0300

    Add change slide function

--- src/pages/test/script.js
+++ src/pages/test/script.js
@@ -1,3 +1,21 @@
 import "./style.scss";
 
-console.log('hi test');
+const next_slide = document.getElementById("next_slide");
+const previous_slide = document.getElementById("previous_slide");
+const slides = document.getElementsByClassName("slide");
+let currentSlide = 0;
+
+function change_slide (change) {
+    slides[currentSlide].classList.add("hidden");
+    if ((currentSlide + change) === (slides.length)) {
+        currentSlide = 0;
+    } else if ((currentSlide + change) < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide += change;
+    }
+    slides[currentSlide].classList.remove("hidden");
+}
+
+next_slide.addEventListener('click', ()=>change_slide(1));
+previous_slide.addEventListener('click', ()=>change_slide(-1));
