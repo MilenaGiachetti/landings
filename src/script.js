7cb9bb39d8971ecfe186d9e47c08773287ec026b
Milena Giachetti <giachettimilena@gmail.com>
2021-04-13 21:48:09 -0300

    Add more info fx base

--- src/script.js
+++ src/script.js
@@ -2,13 +2,42 @@ import "./style.scss";
 
 let cards = document.getElementsByClassName("card");
 function resizeGridItems() {
-    console.log("here2");
     for (let i = 0; i < cards.length; i ++){
         let height = cards[i].querySelector(".card-body").getBoundingClientRect().height;
-        console.log(height);
-        let rows = Math.ceil(height / 20);
+        let rows = Math.ceil(height / 5);
         cards[i].style.gridRowEnd = 'span ' + rows;
     }
 }
 resizeGridItems();
 window.addEventListener("resize",  () => resizeGridItems());
+let fadeIns = [];
+
+for (let i = 0; i < cards.length; i ++){
+    cards[i].addEventListener('click', () => {
+        console.log(fadeIns)
+        if(fadeIns.length > 0) {
+            for (let j = 0; j < fadeIns.length; j ++){
+                console.log(fadeIns[j])
+                fadeIns[j].classList.remove("fadeIn");
+            }
+        }
+        cards[i].querySelector(".card-content").classList.add("fadeIn");
+        cards[i].querySelector(".close").classList.add("fadeIn");
+        fadeIns.length = 0;
+        fadeIns.push(cards[i].querySelector(".card-content"));
+        fadeIns.push(cards[i].querySelector(".close"));
+
+        resizeGridItems();
+    });
+}
+
+let close = document.getElementsByClassName("close");
+
+for (let i = 0; i < close.length; i ++){
+    close[i].addEventListener('click', (e) => {
+        close[i].parentElement.querySelector(".card-content").classList.remove("fadeIn");
+        close[i].classList.remove("fadeIn");
+        resizeGridItems();
+        e.stopPropagation();
+    });
+}
