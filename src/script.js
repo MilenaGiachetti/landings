59c8071415edbe218b1f8343ed1f238ace7d3799
Milena Giachetti <giachettimilena@gmail.com>
2021-04-13 22:53:10 -0300

    Fix: add resize after images loaded

--- src/script.js
+++ src/script.js
@@ -1,43 +1,36 @@
 import "./style.scss";
 
-let cards = document.getElementsByClassName("card");
-function resizeGridItems() {
-    for (let i = 0; i < cards.length; i ++){
-        let height = cards[i].querySelector(".card-body").getBoundingClientRect().height;
-        let rows = Math.ceil(height / 5);
-        cards[i].style.gridRowEnd = 'span ' + rows;
+window.onload = function() {
+    function resizeGridItems() {
+        document.querySelectorAll(".card").forEach(card => {
+            let height = card.querySelector(".card-body").getBoundingClientRect().height;
+            let rows = Math.ceil(height / 5);
+            card.style.gridRowEnd = 'span ' + rows;
+        });
     }
-}
-resizeGridItems();
-window.addEventListener("resize",  () => resizeGridItems());
-let fadeIns = [];
 
-for (let i = 0; i < cards.length; i ++){
-    cards[i].addEventListener('click', () => {
-        console.log(fadeIns)
-        if(fadeIns.length > 0) {
-            for (let j = 0; j < fadeIns.length; j ++){
-                console.log(fadeIns[j])
-                fadeIns[j].classList.remove("fadeIn");
-            }
-        }
-        cards[i].querySelector(".card-content").classList.add("fadeIn");
-        cards[i].querySelector(".close").classList.add("fadeIn");
-        fadeIns.length = 0;
-        fadeIns.push(cards[i].querySelector(".card-content"));
-        fadeIns.push(cards[i].querySelector(".close"));
-
-        resizeGridItems();
+    let fadeIns = [];
+    document.querySelectorAll(".card").forEach(card => {
+        card.addEventListener('click', () => {            
+            fadeIns.forEach(element => element.classList.remove("fadeIn"));
+            card.querySelector(".card-content").classList.add("fadeIn");
+            card.querySelector(".close").classList.add("fadeIn");
+            fadeIns.length = 0;
+            fadeIns.push(card.querySelector(".card-content"));
+            fadeIns.push(card.querySelector(".close"));
+            resizeGridItems();
+        });
     });
-}
 
-let close = document.getElementsByClassName("close");
-
-for (let i = 0; i < close.length; i ++){
-    close[i].addEventListener('click', (e) => {
-        close[i].parentElement.querySelector(".card-content").classList.remove("fadeIn");
-        close[i].classList.remove("fadeIn");
-        resizeGridItems();
-        e.stopPropagation();
+    document.querySelectorAll(".close").forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            btn.parentElement.querySelector(".card-content").classList.remove("fadeIn");
+            btn.classList.remove("fadeIn");
+            resizeGridItems();
+            e.stopPropagation();
+        });
     });
-}
+
+    resizeGridItems();
+    window.addEventListener("resize",  () => resizeGridItems());
+};
